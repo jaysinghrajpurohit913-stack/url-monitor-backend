a998529be9d9cb728e2f292f60c0a7dee6474156
jaysinghrajpurohit913-stack <jaysinghrajpurohit913@gmail.com>
2026-06-19 09:34:01 +0530

    crud

---
+++ src/controller/crud.controller.js
@@ -0,0 +1,51 @@
+const MonitorModel = require('../models/monitor.model');
+
+
+const getmonitors   =  async (req , res )=>{
+     try {
+
+        const monitors = await MonitorModel.find({
+            userId: req.user.id
+        });
+
+        res.json({
+            success: true,
+            data: monitors
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+}
+
+
+const deleteAllMonitor  =  async (req , res )=>{
+     try {
+
+        const monitors = await MonitorModel.findByIdAndDelete({
+            userId: req.user.id
+        });
+
+        res.json({
+            success: true,
+            data: monitors
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+}
+
+
+
+module.exports = {
+    getmonitors,
+    deleteAllMonitor
+
+}
--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -3,12 +3,15 @@ const routes = express.Router();
 
 const UrlValidator = require('../controller/url.controller');
 const auth  = require('../Auth/user.auth');
+const {getmonitors} = require('../controller/check.controller');
 
 routes.post('/url' , auth ,UrlValidator ,(req , res ) =>{
     res.json({
         response: req.userUrl
     });
 })
+
+routes.get('/monitors' , getmonitors);
 
 
 
